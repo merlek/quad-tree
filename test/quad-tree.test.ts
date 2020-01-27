f9c07ac49a53265f0827187e0f92c20d06f87862
Merle Klein <merlek@wolfram.com>
2020-01-27 12:07:39 -0600

    added test

--- test/quad-tree.test.ts
+++ test/quad-tree.test.ts
@@ -1,7 +1,26 @@
+import { Point, Rect } from '../src/data-structures';
+import { QuadTree } from '../src/quad-tree';
+import { randomInt } from 'utility-functions';
+
 // const precision = 9;
 
-// describe('canvas', () => {
-//   afterAll(function() {});
+describe('quad-tree', () => {
+  const size = 1000;
+  const qtree = new QuadTree(
+    new Rect(new Point(size / 2, size / 2), size, size)
+  );
+  afterAll(function() {});
 
-//   it('canvas values are immutable', () => {});
-// });
+  it('quad-tree can insert and query', () => {
+    for (let i = 0; i < size; i++) {
+      qtree.insert(new Point(randomInt(0)(size), randomInt(0)(size)));
+    }
+
+    const point = new Point(randomInt(0)(size), randomInt(0)(size));
+    qtree.insert(point);
+
+    const found = qtree.query(new Rect(point, 10, 10));
+
+    expect(found.includes(point)).toBeTrue();
+  });
+});
